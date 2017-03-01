d4ee376f8584b7d71f7516444ce38b6be1002c1b
Adam Jones <adam.jones@ucla.edu>
2017-03-01 15:33:33 -0800

    Added flower geometry and texture

--- js/app/scene.js
+++ js/app/scene.js
@@ -1,22 +1,31 @@
 Declare_Any_Class( "Scene",  // An example of a displayable object that our class Canvas_Manager can manage.  This one draws the scene's 3D shapes.
   { 'construct': function( context )
       { this.shared_scratchpad    = context.shared_scratchpad;
-        shapes_in_use.branch = new Capped_Cylinder(1, 8);
+        shapes_in_use.branch = new Capped_Cylinder(1, 6);
+        shapes_in_use.flower = new Subdivision_Sphere(3);
         
       },
     'display': function(time)
       {
         var graphics_state  = this.shared_scratchpad.graphics_state,
-            model_transform = mat4();             // We have to reset model_transform every frame, so that as each begins, our basis starts as the identity.
+            branch_model_transform = mat4(),
+            flower_model_transform = mat4();
+
         shaders_in_use[ "Default" ].activate();
 
         //graphics_state.lights.push( new Light( vec4(  30*light_orbit[0],  30*light_orbit[1],  34*light_orbit[0], 1 ), Color( 0, .4, 0, 1 ), 100000 ) );
 
-        var purplePlastic = new Material( Color( 0.5,0.5,0.5,1 ), .4, .4, .8, 40, "stem.png" );
+        var branchMaterial = new Material(Color(0.5, 0.5, 0.5, 1.0), .4, .4, .8, 40, "img/stem.png");
+        var flowerMaterial = new Material(Color(0.5, 0.5, 0.5, 1.0), .4, .4, .8, 40, "img/flower.jpg");
 
-        model_transform = mult(model_transform, rotation(90, vec3(1, 0, 0)));
-        model_transform = mult(model_transform, scale(1, 1, 4));
-        shapes_in_use.branch.draw(graphics_state, model_transform, purplePlastic);
+        branch_model_transform = mult(branch_model_transform, translation(0, -4, 5));
+        branch_model_transform = mult(branch_model_transform, rotation(90, vec3(1, 0, 0)));
+        branch_model_transform = mult(branch_model_transform, scale(1, 1, 6));
+        shapes_in_use.branch.draw(graphics_state, branch_model_transform, branchMaterial);
+
+        flower_model_transform = mult(flower_model_transform, translation(0, 0, 5));
+        flower_model_transform = mult(flower_model_transform, scale(1.5, 1.5, 1.5));
+        shapes_in_use.flower.draw(graphics_state, flower_model_transform, flowerMaterial);
 
       }
   }, Animation );
